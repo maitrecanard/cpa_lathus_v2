62514b510f41f44deac35265bde4e69be0794e5d
maitrecanard <mathieusiaudeau@gmail.com>
2022-12-27 13:52:35 +0100

    maj dashboard

--- src/Controller/Back/MainController.php
+++ src/Controller/Back/MainController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller\Back;
 
+use App\Repository\ScreenContentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin')]
+
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_back_main', methods:['GET'])]
-    public function index(): Response
+    public function index(ScreenContentRepository $screenContentRepository): Response
     {
         return $this->render('back/main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'screenactive' => $screenContentRepository->findBy(['activ' => 1]),
+            'screeninactive' => $screenContentRepository->findBy(['activ' => 0]),
         ]);
     }
 }
